a51fe716f28ebdbb188d76ebbafccb6371bb8e26
Smriti Basnet <esmeb@pdx.edu>
2023-12-03 14:37:40 -0500

    Separation of validation logic

--- server/sequelize-db/queries.js
+++ server/sequelize-db/queries.js
@@ -1,6 +1,12 @@
 const { DataTypes } = require('sequelize');
 
 module.exports = function (sequelize) {
+  function validateChartType(chartData) {
+    if (chartData && typeof chartData.chartType !== 'string') {
+      throw new Error('chart.chartType must be a string');
+    }
+  }
+
   const Queries = sequelize.define(
     'Queries',
     {
@@ -19,23 +25,12 @@ module.exports = function (sequelize) {
       queryText: {
         type: DataTypes.TEXT,
       },
-      // With addition of multiple queries per query...
-      // unsure what direction charts will go.
-      // Leaving this as a JSON object
+
       chart: {
         type: DataTypes.JSON,
         validate: {
           matchesShape(value) {
-            if (value && typeof value.chartType !== 'string') {
-              throw new Error('chart.chartType must be a string');
-            }
-            // value.fields could also exist
-            // it is an object map of { fieldname: value }
-            // These are a mix of <chartField>:<resultColumn>, and <chartOption>:<value>
-            // This will probably become more formalized if charting is revisted
-            // Because Dates, null, Arrays, and objects are "objects" in JS,
-            // we won't bother checking for it.
-            // This is more for documentation and this is a nice place to put it.
+            validateChartType(chartData);
           },
         },
       },
